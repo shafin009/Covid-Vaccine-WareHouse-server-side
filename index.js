6898b0d7720e48ec7322e9db7fe99985ae5bbb5d
shafin009 <ghostboyshafin@example.com>
2022-05-01 19:29:09 +0600

    data-inserted

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 const express = require('express');
+const cors = require('cors');
+const { ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+var MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+
 
 
 //----Middleware---
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+var uri = "mongodb://covid-data:covid12345@vaccine77-shard-00-00.bmpdq.mongodb.net:27017,vaccine77-shard-00-01.bmpdq.mongodb.net:27017,vaccine77-shard-00-02.bmpdq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yz5qv3-shard-0&authSource=admin&retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+async function run() {
+
+    try {
+
+        await client.connect();
+        const productCollection = client.db("vaccine").collection("item");
+        // Get API
+
+        app.get("/item", async (req, res) => {
+
+            const query = {};
+            const cursor = productCollection.find(query);
+            const vaccine = await cursor.toArray();
+            res.send(vaccine);
+
+
+
+            app.get("/item/:id", async (req, res) => {
+                const id = req.params.id;
+                const query = { _id: ObjectId(id) };
+                const vaccine = await productCollection.findOne(query);
+                res.send(vaccine);
+                console.log
+            });
+
+
+
+        });
+
+
+
+
+
+    } finally { }
+
+}
+run().catch(console.dir);
 
 
 
