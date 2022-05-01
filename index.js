2c9dc5aeb78ac42d29b62d9b32eccc88f06c6f7c
shafin009 <ghostboyshafin@example.com>
2022-05-01 09:54:25 +0600

    'shafin'

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+//----Middleware---
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Vaccine giving')
+})
+
+app.listen(port, () => {
+    console.log('running port', port)
+})
+
+
+
+
+
