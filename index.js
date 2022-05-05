e76864415becb8e1ade6d236ca2459e7c89fe965
shafin009 <ghostboyshafin@example.com>
2022-05-05 22:22:46 +0600

    'addedJwt-

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ var MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
-
 //----Middleware---
 app.use(cors());
 app.use(express.json());
 
 
 
+//------------JWT Token Function---------
+function token(req, res, next) {
+    const headerAuth = req.headers.authorization;
+    if (!headerAuth) {
+        return res.status(401).send({ message: "Sorry! Access Denied" });
+    }
+    const jwtToken = headerAuth.split(' ')[1];
+    jwt.verify(jwtToken, process.env.JWT_TOKEN, (err, decoded) => {
+
+        if (err) {
+            return res.status(403).send({ message: "Sorry! Wrong Access" });
+        }
+        req.decoded = decoded;
+    })
+    next()
+}
 
 
 var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@vaccine77-shard-00-00.bmpdq.mongodb.net:27017,vaccine77-shard-00-01.bmpdq.mongodb.net:27017,vaccine77-shard-00-02.bmpdq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yz5qv3-shard-0&authSource=admin&retryWrites=true&w=majority`;
@@ -28,6 +42,7 @@ async function run() {
         const vaccineCollection = client.db("vaccine").collection("item");
 
 
+        //------------login with JWT---------
         app.post('/login', async (req, res) => {
             const user = req.body
             const tokenAccess = jwt.sign(user, process.env.JWT_TOKEN, {
@@ -39,7 +54,7 @@ async function run() {
 
 
 
-        // Get API
+        // ----------Get API----------
         app.get("/item", async (req, res) => {
 
             const query = {};
@@ -62,7 +77,7 @@ async function run() {
             res.send(result);
         });
 
-        // delete Api
+        // ----------Delete API----------
         app.delete("/item/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -71,22 +86,25 @@ async function run() {
         });
 
 
-        app.get("/myitem", async (req, res) => {
+        
+        //------------my Item with JWT---------
+        app.get("/myitem", token, async (req, res) => {
+            const emailDecoded = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = vaccineCollection.find(query);
-            const vaccine = await cursor.toArray();
-            res.send(vaccine);
+            if (emailDecoded === email) {
+                const query = { email: email };
+                const cursor = vaccineCollection.find(query);
+                const vaccine = await cursor.toArray();
+                res.send(vaccine);
+            }
+            else {
+                return res.status(403).send({ message: "Sorry! Wrong Access, Goodbye!" });
+            }
         });
 
-        app.post("/myitem", async (req, res) => {
-            const newItem = req.body;
-            const result = await vaccineCollection.insertOne(newItem);
-            res.send(result);
 
-        });
 
-        // update user
+        //------------update user quantity------------------
         app.put("/item/:id", async (req, res) => {
             const id = req.params.id;
             const updateVaccine = req.body;
